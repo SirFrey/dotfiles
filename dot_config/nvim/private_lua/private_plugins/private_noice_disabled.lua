4065de3c360d20a72618dcd50227a445d70a59fc
Moises <moisescastellano12312@gmail.com>
2025-07-29 10:20:03 -0400

    Remove .config/nvim/lua/plugins/coloscheme.lua Remove .config/nvim/lua/plugins/fidget.lua Remove .config/nvim/lua/plugins/transparent.lua Add .config/nvim/lua/plugins/coloscheme.lua Add .config/nvim/lua/plugins/fidget.lua Add .config/nvim/lua/plugins/transparent.lua Change attributes of .config/nvim/LICENSE Change attributes of .config/nvim/README.md Change attributes of .config/nvim/.gitignore Change attributes of .config/nvim/.neoconf.json Change attributes of .config/nvim/lazy-lock.json Change attributes of .config/nvim/lazyvim.json Change attributes of .config/nvim/init.lua Change attributes of .config/nvim/lua/config/keymaps.lua Change attributes of .config/nvim/lua/config/lazy.lua Change attributes of .config/nvim/lua/config/options.lua Change attributes of .config/nvim/lua/config/vimrc.vim Change attributes of .config/nvim/lua/plugins/avante.lua Change attributes of .config/nvim/lua/plugins/cmp.lua Change attributes of .config/nvim/lua/plugins/coding.lua Change attributes of .config/nvim/lua/plugins/conform.lua Change attributes of .config/nvim/lua/plugins/copilot.lua Change attributes of .config/nvim/lua/plugins/gitconflicts.lua Change attributes of .config/nvim/lua/plugins/harpoon.lua Change attributes of .config/nvim/lua/plugins/linting.lua Change attributes of .config/nvim/lua/plugins/lsp.lua Change attributes of .config/nvim/lua/plugins/lspkind.lua Change attributes of .config/nvim/lua/plugins/lualine.lua Change attributes of .config/nvim/lua/plugins/markdown.lua Change attributes of .config/nvim/lua/plugins/mdx_config.lua Change attributes of .config/nvim/lua/plugins/noice_disabled.lua Change attributes of .config/nvim/lua/plugins/nvim_spectre.lua Change attributes of .config/nvim/lua/plugins/oil.lua Change attributes of .config/nvim/lua/plugins/presence.lua Change attributes of .config/nvim/lua/plugins/schemas.lua Change attributes of .config/nvim/lua/plugins/tailwind.lua Change attributes of .config/nvim/lua/plugins/telescope.lua Change attributes of .config/nvim/lua/plugins/tmux_nav.lua Change attributes of .config/nvim/lua/plugins/trouble.lua Change attributes of .config/nvim/lua/plugins/undotree.lua Change attributes of .config/nvim/lua/plugins/vimbegood.lua Change attributes of .config/nvim/stylua.toml

--- dot_config/nvim/private_lua/private_plugins/private_noice_disabled.lua
+++ dot_config/nvim/private_lua/private_plugins/private_noice_disabled.lua
@@ -2,15 +2,12 @@ return {
   {
     -- TODO: Put some backgronds of color CmpNormal
     "folke/noice.nvim",
-    event = "VeryLazy",
-    enabled = false,
+    enabled = true,
     config = function()
       require("lualine").setup({
         sections = {
           lualine_x = {
             {
-              require("noice").api.statusline.mode.get,
-              cond = require("noice").api.statusline.mode.has,
               color = { fg = "#ff9e64" },
             }
           },
