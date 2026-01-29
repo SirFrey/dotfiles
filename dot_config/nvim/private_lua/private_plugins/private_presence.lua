64e4ba6acc3e48454758b568bf63e6fb44f00488
Moises Castellanos <moisescastellano12312@gmail.com>
2026-01-29 12:53:43 -0400

    Remove ..private_dot_gitconfig.toml Update .chezmoi.toml Add .chezmoidata/data.toml Update .config/nvim/lua/plugins/presence.lua Update .gitconfig

--- dot_config/nvim/private_lua/private_plugins/private_presence.lua
+++ dot_config/nvim/private_lua/private_plugins/private_presence.lua
@@ -1,30 +1,15 @@
 return {
   "vyfor/cord.nvim",
-  event = "VeryLazy",
   build = ":Cord update",
+  event = "VeryLazy",
   config = function()
     require("cord").setup({
-      editor = {
-        tooltip = "Nice Editor",
-      },
-      idle = {
-        timeout = 600000,
-        details = "Away From You",
-      },
-      text = {
-        editing = function(opts)
-          return "Editing " .. opts.filename
-        end, -- or use a string value like 'Editing a file' if you don't want it to be dynamic
-        -- etc ...
-      },
-      log_level = "trace", -- one of 'trace', 'debug', 'info', 'warn', 'error'
-      advanced = {
-        server = {
-          pipe_path = "/tmp/discord-ipc-0",
-        },
-      },
+     -- log_level = "trace", -- one of 'trace', 'debug', 'info', 'warn', 'error'
+     -- advanced = {
+     --   server = {
+     --     pipe_path = "/tmp/discord-ipc-0",
+     --   },
+     -- },
     })
   end,
-  -- opts = {}
-  --
 }
